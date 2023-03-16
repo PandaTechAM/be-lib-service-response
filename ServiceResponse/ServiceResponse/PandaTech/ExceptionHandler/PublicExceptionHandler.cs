0f8df053d181ba97f513b2f4103bc55917f256a2
DefGh <defghq@gmail.com>
2023-03-16 12:32:17 +0400

    PublicExceptionHandler empruvment

--- ServiceResponse/ServiceResponse/PandaTech/ExceptionHandler/PublicExceptionHandler.cs
+++ ServiceResponse/ServiceResponse/PandaTech/ExceptionHandler/PublicExceptionHandler.cs
@@ -5,7 +5,7 @@ public class PublicExceptionHandler : IExceptionHandler
     public ServiceResponse Handle(ServiceResponse serviceResponse, Exception serviceException)
     {
         serviceResponse.Success = false;
-        serviceResponse.Message = serviceException.InnerException?.Message ?? serviceException.Message;
+        serviceResponse.Message = "An error occurred, please contact support.";
         serviceResponse.ResponseStatus = ServiceResponseStatus.Error;
         return serviceResponse;
     }
