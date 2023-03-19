9e40e94d253156d5c45f4a1f94e72da8bf8ecfe1
Haik <106117370+HaikAsatryan@users.noreply.github.com>
2023-03-20 01:33:21 +0400

    Update ServiceResponse.cs
    Signed-off-by: Haik <106117370+HaikAsatryan@users.noreply.github.com>

--- ServiceResponse/ServiceResponse/PandaTech/ServiceResponse/ServiceResponse.cs
+++ ServiceResponse/ServiceResponse/PandaTech/ServiceResponse/ServiceResponse.cs
@@ -1,17 +1,6 @@
 ﻿namespace PandaTech.ServiceResponse;
 
-/*
- * This is service response template for .NET 6, 7 web api projects. This template is based on the best practices and
- * has goal to harmonize all API I/O operations. The benefit of this template over other templates is that it totally
- * integrates with OpenAPI and Swagger. So, IActionResults and other services,
- * response will be visible in Swagger UI and loads of other features are and is going to be included.
- * 
- * This Template is designed by PandaTech LLC.
- * We build software with the greatest quality!
- * Our website: www.pandatech.it :)
- */
 
-//See below class for every type of service responses in standardized way:
 public class ServiceResponse : IServiceResponse
 {
     public bool Success { get; set; } = true;
