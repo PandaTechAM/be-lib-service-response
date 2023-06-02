687bb5c03f2b3f6a12d31e6cc4f2b279598eba29
DefGh <defghq@gmail.com>
2023-06-02 14:05:41 +0400

    Update ServiceResponse.cs
    Signed-off-by: DefGh <defghq@gmail.com>

--- ServiceResponse/ServiceResponse/PandaTech/ServiceResponse/ServiceResponse.cs
+++ ServiceResponse/ServiceResponse/PandaTech/ServiceResponse/ServiceResponse.cs
@@ -1,18 +1,11 @@
 ﻿namespace PandaTech.ServiceResponse;
 
 
-public class ServiceResponse : IServiceResponse
+public class ServiceResponse
 {
     public bool Success { get; set; } = true;
     public string Message { get; set; } = string.Empty;
     public ServiceResponseStatus ResponseStatus { get; set; } = ServiceResponseStatus.Ok;
-}
-
-public interface IServiceResponse
-{
-    bool Success { get; set; }
-    string Message { get; set; }
-    ServiceResponseStatus ResponseStatus { get; set; }
 }
 
 public class ServiceResponse<T> : ServiceResponse
