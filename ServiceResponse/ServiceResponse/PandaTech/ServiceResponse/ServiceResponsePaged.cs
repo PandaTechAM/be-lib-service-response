15a842b8e2c5d7b5d4c9c3cc559fb9bb2e0e5323
Ruben Bisharyan <ruben.bisharyan@pandatech.it>
2023-10-20 12:30:30 +0400

    Make aggregate object?

--- ServiceResponse/ServiceResponse/PandaTech/ServiceResponse/ServiceResponsePaged.cs
+++ ServiceResponse/ServiceResponse/PandaTech/ServiceResponse/ServiceResponsePaged.cs
@@ -4,7 +4,7 @@ public class ServiceResponsePaged<T> : ServiceResponse
 {
     public ResponseDataPaged<T> ResponseData { get; set; } = new();
 
-    public ServiceResponsePaged(List<T> data, int page, int pageSize, int totalCount, Dictionary<string, object>? aggregates = null)
+    public ServiceResponsePaged(List<T> data, int page, int pageSize, int totalCount, Dictionary<string, object?>? aggregates = null)
     {
         ResponseData = new ResponseDataPaged<T>(data, page, pageSize, totalCount, aggregates);
     }
@@ -25,9 +25,9 @@ public class ResponseDataPaged<T>
     public int Page { get; set; } = 1;
     public int PageSize { get; set; } = 20;
     public long TotalCount { get; set; } = 0;
-    public Dictionary<string, object>? Aggregates { get; set; }
+    public Dictionary<string, object?>? Aggregates { get; set; }
 
-    public ResponseDataPaged(List<T> data, int page, int pageSize, long totalCount, Dictionary<string, object>? aggregates)
+    public ResponseDataPaged(List<T> data, int page, int pageSize, long totalCount, Dictionary<string, object?>? aggregates)
     {
         Data = data;
         Page = page;
